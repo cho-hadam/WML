8085f7efea106d2066d910f2455e4571b467b55c
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-06-24 03:02:35 +0900

    SubActivity.java 화면 전환 추가

--- app/src/main/java/com/jmhd/wml/SubActivity.java
+++ app/src/main/java/com/jmhd/wml/SubActivity.java
@@ -2,13 +2,28 @@ package com.jmhd.wml;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SubActivity extends AppCompatActivity {
+    private Button btn_calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        btn_calendar = (Button) findViewById(R.id.btn_calendar);
+        btn_calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SubActivity.this, MainActivity.class);
+                intent.addFlags(intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+                overridePendingTransition(0,0);
+            }
+        });
     }
 }
