1f17232a9bac307c3d691a88abb40bbbe887456b
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-06-24 01:35:31 +0900

    Override error

--- app/src/main/java/com/jmhd/wml/MainActivity.java
+++ app/src/main/java/com/jmhd/wml/MainActivity.java
@@ -1,8 +1,9 @@
 package com.jmhd.wml;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +12,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn_diary = (Button) findViewById(R.id.btn_diary);
+        btn_diary.setOnClickListener(new View.onClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(this, SubActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     public void goDiary(View view) {
-        Intent intent = new Intent(this, SubActivity.class);
-        startActivity(intent);
+
     }
 }
