3e7f78a8a06d3d68d31acd32b85763e28b063caa
0127jimin <ckdgh4662@naver.com>
2020-06-24 22:22:08 +0900

    ListView와 들어가는 data를 adapter를 이용해 연결함(sub) 확임바람

--- app/src/main/java/com/jmhd/wml/SubActivity.java
+++ app/src/main/java/com/jmhd/wml/SubActivity.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SubActivity extends AppCompatActivity {
     private Button btn_calendar;
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +31,23 @@ public class SubActivity extends AppCompatActivity {
                 overridePendingTransition(0,0);
             }
         });
+
+        list = (ListView)findViewById(R.id.diary_list);
+        List<String> data = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
+
+        data.add("점심 두 그릇 먹은 날");
+        data.add("제목 못 정한 날");
+        data.add("저녁은 치킨");
+        data.add("다연이랑 데이트");
+        data.add("체육 너무 힘든 날");
+        data.add("증명사진 찍으러 간날");
+        data.add("프로젝트 시작");
+        data.add("치킨 먹고 싶다");
+        data.add("청년다방 맛있다");
+        data.add("힘든 지민이");
+        adapter.notifyDataSetChanged();
     }
 }
