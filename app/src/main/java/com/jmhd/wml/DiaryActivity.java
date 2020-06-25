9406de820695519b4e53de101b300095cef5218f
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-06-26 02:12:31 +0900

    Diary에서 뒤로가기 버튼 액션 추가

--- app/src/main/java/com/jmhd/wml/DiaryActivity.java
+++ app/src/main/java/com/jmhd/wml/DiaryActivity.java
@@ -2,7 +2,6 @@ package com.jmhd.wml;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,7 +13,15 @@ public class DiaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
+
         btn_back = (ImageButton)findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(backDiary);
     }
-    btn_back.OnClickListener
+
+    Button.OnClickListener backDiary = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            DiaryActivity.this.finish();
+        }
+    };
 }
