8ba72e9dd094a2a5a65c9db365d6af06591ac41a
0127jimin <ckdgh4662@naver.com>
2020-06-26 02:07:32 +0900

    한거 보내봐

--- app/src/main/java/com/jmhd/wml/DiaryActivity.java
+++ app/src/main/java/com/jmhd/wml/DiaryActivity.java
@@ -2,13 +2,19 @@ package com.jmhd.wml;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class DiaryActivity extends AppCompatActivity {
-
+    private ImageButton btn_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
+        btn_back = (ImageButton)findViewById(R.id.btn_back);
     }
+    btn_back.OnClickListener
 }
