b9f6ce13b4abf77e6c45217b26bbd7ef6413b610
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-06-26 00:54:29 +0900

    setDate 작성

--- app/src/main/java/com/jmhd/wml/MainActivity.java
+++ app/src/main/java/com/jmhd/wml/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private Button btn_diary;
+    private TextView text_month;
 
     public MainActivity() {
     }
@@ -20,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
 
         btn_diary = (Button) findViewById(R.id.btn_diary);
         btn_diary.setOnClickListener(goSub);
+
+        text_month = (TextView) findViewById(R.id.text_month);
+        setDate(text_month);
     }
 
     Button.OnClickListener goSub = new View.OnClickListener() {
@@ -32,4 +40,27 @@ public class MainActivity extends AppCompatActivity {
             MainActivity.this.finish();
         }
     };
+
+    protected void setDate(TextView text_month) {
+        String[] month_en = {
+                "Junuary",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+        };
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int get_month = calendar.get(Calendar.MONTH);
+        String month = month_en[get_month];
+
+        text_month.setText(month + " " + String.valueOf(year));
+    }
 }
