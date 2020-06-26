b510b8a50954d66b77d5303b895c0f0eaab553df
0127jimin <ckdgh4662@naver.com>
2020-06-27 00:47:17 +0900

    menu버튼

--- app/src/main/java/com/jmhd/wml/DiaryActivity.java
+++ app/src/main/java/com/jmhd/wml/DiaryActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class DiaryActivity extends AppCompatActivity {
     private ImageButton btn_back;
+    private LinearLayout layout;
+    private ImageButton btn_menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +19,10 @@ public class DiaryActivity extends AppCompatActivity {
 
         btn_back = (ImageButton)findViewById(R.id.btn_back);
         btn_back.setOnClickListener(backDiary);
+        btn_menu = (ImageButton)findViewById(R.id.btn_menu);
+        btn_menu.setOnClickListener(menu);
+        layout = (LinearLayout)findViewById(R.id.LinearLayout_menu);
+        layout.setVisibility(View.GONE);
     }
 
     Button.OnClickListener backDiary = new View.OnClickListener() {
@@ -24,4 +31,12 @@ public class DiaryActivity extends AppCompatActivity {
             DiaryActivity.this.finish();
         }
     };
+    Button.OnClickListener menu = new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            layout.setVisibility(View.VISIBLE);
+        }
+    };
+
+
 }
