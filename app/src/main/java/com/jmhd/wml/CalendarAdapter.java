52dda65d0489b3a08ef5bd6a81e8acf56a8665f6
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-06-26 23:53:01 +0900

    calendar adapter getView 코드 작성 중

--- app/src/main/java/com/jmhd/wml/CalendarAdapter.java
+++ app/src/main/java/com/jmhd/wml/CalendarAdapter.java
@@ -1,13 +1,18 @@
 package com.jmhd.wml;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class CalendarAdapter extends BaseAdapter {
     // activity 참조
     Context context = null;
+    TextView date = null;
 
     public CalendarAdapter(Context context) {
         this.context = context;
@@ -19,17 +24,35 @@ public class CalendarAdapter extends BaseAdapter {
     }
 
     @Override
-    public Object getItem(int i) {
+    public Object getItem(int position) {
         return null;
     }
 
     @Override
-    public long getItemId(int i) {
-        return 0;
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(int position, View convertView, ViewGroup parent) {
+        int maxDate = getMaxDate();
+
+        if (convertView == null) {
+            for (int i = 1; i <= maxDate; i++) {
+                date = new TextView(context);
+                date.setText(i);
+                Log.d("day", String.valueOf(i));
+            }
+        } else {
+            date = (TextView) convertView;
+        }
+
+        return date;
+    }
+
+    private int getMaxDate() {
+        Calendar calendar = Calendar.getInstance();
+        int maxDate = calendar.getActualMaximum(Calendar.DATE);
+        return maxDate;
     }
 }
