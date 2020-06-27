fe08b473624a9f1e6e10ddfe84c4455b4de84a02
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-06-28 03:13:51 +0900

    0일 누르면 안 눌리게

--- app/src/main/java/com/jmhd/wml/DateClickListener.java
+++ app/src/main/java/com/jmhd/wml/DateClickListener.java
@@ -2,6 +2,7 @@ package com.jmhd.wml;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -11,21 +12,26 @@ import androidx.core.content.ContextCompat;
 public class DateClickListener implements View.OnClickListener {
     private Context context;
     private LinearLayout activeBox;
-    private TextView date;
     private LinearLayout round;
+    private TextView date;
+    private String dateinfo;
 
-    public DateClickListener(Context context, TextView date) {
+    public DateClickListener(Context context, TextView date, String dateinfo) {
         this.context = context;
         this.activeBox = (LinearLayout) ((Activity)context).findViewById(R.id.active_box);
-        this.date = date;
         this.round = (LinearLayout) date.getParent();
+        this.date = date;
+        this.dateinfo = dateinfo+date.getText();
+        Log.d("MyLog",this.dateinfo);
     }
 
     @Override
     public void onClick(View view) {
         if(activeBox.getVisibility() == View.INVISIBLE) {
-            activeBox.setVisibility(View.VISIBLE);
-            round.setBackground(ContextCompat.getDrawable(context, R.drawable.round));
+            if (!date.getText().equals("0")) {
+                activeBox.setVisibility(View.VISIBLE);
+                round.setBackground(ContextCompat.getDrawable(context, R.drawable.round));
+            }
         } else {
             activeBox.setVisibility(View.INVISIBLE);
             round.setBackground(null);
---
+++ app/src/main/java/com/jmhd/wml/DateInfo.java
@@ -0,0 +1,17 @@
+package com.jmhd.wml;
+
+public class DateInfo {
+    private String date;
+
+    public DateInfo(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
