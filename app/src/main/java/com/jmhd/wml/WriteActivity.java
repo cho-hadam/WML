7d00b448594a9f9ba1a041fe11e9fd9f2138472a
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-07-04 23:59:04 +0900

    Write 화면 상단에 날짜 출력

--- app/src/main/java/com/jmhd/wml/WriteActivity.java
+++ app/src/main/java/com/jmhd/wml/WriteActivity.java
@@ -8,12 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class WriteActivity extends AppCompatActivity {
     private ImageButton btn_save;
     private EditText input_title;
     private EditText input_content;
     private DateInfo dateInfo;
+    private TextView text_write_date;
 
 
    // DBHelper db = new DBHelper(getApplicationContext(), "WML.db", null, 1);
@@ -24,12 +26,15 @@ public class WriteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
 
-        btn_save = (ImageButton) findViewById(R.id.btn_save);
-        btn_save.setOnClickListener(save);
-
         // 누른 날짜 알기 위해서 DateInfo 클래스 받아옴
         Intent intent = getIntent();
         dateInfo = (DateInfo) intent.getSerializableExtra("dateInfo");
+
+        text_write_date = (TextView) findViewById(R.id.text_write_date);
+        text_write_date.setText(dateInfo.toString());
+
+        btn_save = (ImageButton) findViewById(R.id.btn_save);
+        btn_save.setOnClickListener(save);
     }
 
     Button.OnClickListener save = new View.OnClickListener(){
